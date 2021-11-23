32115dc0b760dda083d2e54e7a1e09d476532645
MAVERICKdesu <48190864+MAVERICKdesu@users.noreply.github.com>
2021-11-24 00:44:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from aiocqhttp.event import Event
 from nonebot.default_config import *
 
 
-SUPERUSERS = {714650324}#1
+SUPERUSERS = {114514}#1
 COMMAND_START = {'', '/', '!', '／', '！'}
 HOST = '0.0.0.0'
 PORT = 8080
