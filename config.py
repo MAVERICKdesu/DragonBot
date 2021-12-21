e12155d7a808cd4eed2f34da80ad765fc649a765
MAVERICKdesu <48190864+MAVERICKdesu@users.noreply.github.com>
2021-12-21 21:31:57 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from types import FunctionType
 from aiocqhttp.event import Event
 from nonebot.default_config import *
+from os import path
 
 
 SUPERUSERS = {114514}#1
@@ -8,7 +9,8 @@ COMMAND_START = {'', '/', '!', '／', '！'}
 HOST = '0.0.0.0'
 PORT = 8080
 
-SCORCE_IMG_PATH = "images/"
-SCORCE_VOICE_PATH = "voices/"
-SCORCE_VIDEO_PATH = "videos/"
+BASE_PATH = ""
+SCORCE_IMG_PATH = path.join(BASE_PATH, "images")
+SCORCE_VOICE_PATH = path.join(BASE_PATH, "voices")
+SCORCE_VIDEO_PATH = path.join(BASE_PATH, "videos")
 PLUGINS_PATH = "plugins/"
